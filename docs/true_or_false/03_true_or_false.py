22845c314fc1af7fc4ec1ff8201575314d7a83ba
Sebastian Flores <sebastiandres@gmail.com>
2023-08-07 15:36:58 -0400

    Using functions now that it is possible

--- docs/true_or_false/03_true_or_false.py
+++ docs/true_or_false/03_true_or_false.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import streamlit_book as stb
 
-st.title("True or False Question")
-st.header("An example")
+def my_private_function_1():
+    st.title("True or False Question")
+    st.header("An example")
 
-# To control the width of the text outputs
-show_code = st.checkbox("Show code?")
-with stb.echo("below", show_code):
-    stb.true_or_false('Is "Indiana Jones and the Last Crusade" the best movie of the trilogy?', 
-                        True, 
-                        success="You have chosen wisely", 
-                        error="You have chosen poorly", 
-                        button="You must choose")
+def my_private_function_2():
+    show_code = st.checkbox("Show code?")
+    with stb.echo("below", show_code):
+        stb.true_or_false('Is "Indiana Jones and the Last Crusade" the best movie of the trilogy?', 
+                            True, 
+                            success="You have chosen wisely", 
+                            error="You have chosen poorly", 
+                            button="You must choose")
+
+my_private_function_1()
+my_private_function_2()
